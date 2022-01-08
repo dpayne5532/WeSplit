6b14479d9c1703ae623b035781773650cb352edf
Dan Payne <dpayne5532@gmail.com>
2022-01-08 13:52:05 -0700

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Dan Payne on 1/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var tapCount = 0
+    
+    var body: some View {
+        
+        
+        
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
